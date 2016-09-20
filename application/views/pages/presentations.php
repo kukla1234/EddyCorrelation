c1c1e7a60da91502719af70ed2d9d0c8ae87e016
ekukla <elizabeth.kukla@trivago.com>
2016-09-20 15:00:46 +0200

    fix presentation page links

--- application/views/pages/presentations.php
+++ application/views/pages/presentations.php
@@ -22,33 +22,33 @@
 
         <div class='berg_weblink berg_align-center'>
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Instrument%20animation.ppt" target="_blank">Instrument animation</a>
+                <a href="/berg/slides/Instrument%20animation.ppt" target="_blank">Instrument animation</a>
                     – Animations of our typical instrument for oxygen flux measurements and how it records data.
                     For more information, see <a href="/berg/journals/Berg%20and%20Huettel%202008%20Oceanography.pdf" target="_blank">Berg and Huettel (2008)</a>
             </p>
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Instrument%20illustration.ppt>">Instrument illustration</a>
+                <a href="/berg/slides/Instrument%20illustration.ppt">Instrument illustration</a>
                 – Illustration of an instrument on the seafloor and its footprint.
             </p>
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Visible%20eddy%20correlation%20in%20data%201.ppt">Visible eddy covariance in data 1</a>
+                <a href="/berg/slides/Visible%20eddy%20correlation%20in%20data%201.ppt">Visible eddy covariance in data 1</a>
                 – Real data with visible covariance between the vertical velocity and oxygen concentration that gives the flux. 
             </p>
 
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Visible%20eddy%20correlation%20in%20data%202.pp">Visible eddy covariance in data 2</a>
+                <a href="/berg/slides/Visible%20eddy%20correlation%20in%20data%202.ppt">Visible eddy covariance in data 2</a>
                  – Same as above but with cumulative flux calculation added.
             </p>
 
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Eddy%20correlation%20footprint.ppt">Eddy covariance footprint</a>
+                <a href="/berg/slides/Eddy%20correlation%20footprint.ppt">Eddy covariance footprint</a>
                 – The area on the seafloor that gives the measured eddy flux. For more information, see
                 <a href="/berg/journals/Berg%20et%20al%202007%20L&amp;O.pdf" target="_blank">Berg et al. (2007)</a> and
                 <a href="/berg/journals/Rheuban%20and%20Berg%202013%20L&amp;OM.pdf" target="_blank">Rheuban and Berg (2013)</a>.
             </p>
 
             <p class='berg_citation-text text berg_indent-block'>
-                <a href="/berg/img/slides/Transport%20by%20turbulent%20eddy.ppt">Transport by turbulent eddy</a>
+                <a href="/berg/slides/Transport%20by%20turbulent%20eddy.ppt">Transport by turbulent eddy</a>
                 – Illustration of an eddy that brings oxygen down to the seafloor.
             </p>
         </div>
